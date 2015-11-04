d76d5a78dbda269bdee3331d2e26cc93ad37a81e
Phuoc Ngo <jadanngo@gmail.com>
2015-11-03 23:51:33 -0800

    Map toString

--- src/edu/cpp/cs/cs141/classproject/Map.java
+++ src/edu/cpp/cs/cs141/classproject/Map.java
@@ -5,4 +5,13 @@ public class Map {
 	public static final int GRID_SIZE = 4;
 	
 	private Object[][] grid = new Object[GRID_SIZE][GRID_SIZE];
+	
+	
+	/* Show the map to the Player
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString(){
+		return null;
+		
+	}
 }
