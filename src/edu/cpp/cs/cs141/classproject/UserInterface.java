a06c5bf4d64b4a4d07dd0367cf05271ce6e13312
Bryan <Bryan@134.71.151.63>
2015-11-03 23:53:59 -0800

    Javadoc fixes

--- src/edu/cpp/cs/cs141/classproject/UserInterface.java
+++ src/edu/cpp/cs/cs141/classproject/UserInterface.java
@@ -12,23 +12,34 @@ public class UserInterface {
 	}
 
 	/**
-	 * Welcomes the user with {@link #printWelcomeMessage()}
-	 * Starts the game prompts the user with {@link #mainMenu()}
+	 * Welcomes the user with {@link #printWelcomeMessage()} Starts the game
+	 * prompts the user with {@link #mainMenu()}
 	 */
 	public void startGame() {
 
 	}
 
+	/**
+	 * Prints the welcome phrase to the user, letting them know what game they
+	 * are playing.
+	 */
 	private void printWelcomeMessage() {
 
 	}
 
+	/**
+	 * Prints the main menu to the user and prompts for their input.
+	 * 
+	 * @return an integer relating to the input that they chose.
+	 */
 	private int mainMenu() {
 		return -1;
 	}
 
 	/**
-	 * Prints our map. Loops the game.
+	 * Prints the game map and prompts the user for input on what moves he/she
+	 * would like to make. It then cycles through the game loop until the game
+	 * returns that it is finished.
 	 */
 	public void playGame() {
 
