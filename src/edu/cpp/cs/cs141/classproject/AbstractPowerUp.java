723ed583d99153625d132d8da2df4ff0fcd99a8b
Bryan <Bryan@10.110.86.27>
2015-11-04 10:41:26 -0800

    Added javadoc for effect

--- src/edu/cpp/cs/cs141/classproject/AbstractPowerUp.java
+++ src/edu/cpp/cs/cs141/classproject/AbstractPowerUp.java
@@ -2,7 +2,8 @@ package edu.cpp.cs.cs141.classproject;
 
 public abstract class AbstractPowerUp {
 	/**
-	 * 
+	 * This method is an abstract method that will produce the desired effect on
+	 * the player.
 	 */
 	public abstract void effect(Player p);
 }
