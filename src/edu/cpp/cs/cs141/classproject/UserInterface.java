a763157c48175b30ee3c4bf5a659eecbf9c6caf6
Bryan <Bryan@134.71.151.63>
2015-11-03 23:08:39 -0800

    Added the first bits to UI class

--- src/edu/cpp/cs/cs141/classproject/UserInterface.java
+++ src/edu/cpp/cs/cs141/classproject/UserInterface.java
@@ -1,6 +1,28 @@
 package edu.cpp.cs.cs141.classproject;
 
-public class UserInterface {
+import java.util.Scanner;
 
+public class UserInterface {
+private Game gameEngine;
+private Scanner userinput;
+public UserInterface(Game gameEngine){
+	this.gameEngine = gameEngine;
+	userinput = new Scanner(System.in);
+}
+public void startGame(){
 	
+}
+
+private void printWelcomeMessage(){
+	
+}
+
+private int mainMenu(){
+	
+}
+
+public void playGame(){
+	
+}
+
 }
