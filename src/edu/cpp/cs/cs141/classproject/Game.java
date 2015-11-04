d98c35a9f1016e4b5588a74fd7e7d97f383454a7
Thingon <anthonylnguyen97@gmail.com>
2015-11-03 22:58:36 -0800

    added javadoc for game class

--- src/edu/cpp/cs/cs141/classproject/Game.java
+++ src/edu/cpp/cs/cs141/classproject/Game.java
@@ -6,16 +6,55 @@ package edu.cpp.cs.cs141.classproject;
 public class Game {
 	private Map gameMap;
 	private boolean isFinished;
+	private boolean win;
+	/**
+	 * The amount of moves steps in the game.
+	 */
+	private int moveCount;
 
+	/**
+	 * Moves the character
+	 * 
+	 * @param row
+	 * @param col
+	 */
 	public void move(int row, int col) {
 
 	}
 
+	/**
+	 * Tests if {@link edu.cpp.cs.cs141.classproject.Player#getNumBullets(int)}
+	 * is greater than 0. Then subtracts it by one. Removes an assassin from the
+	 * map if there is one in front of the player.
+	 */
 	public void shoot() {
-
-		
 	}
 
-	public void generateRooms(){
+	/**
+	 * Generates the rooms and randomly makes only one of the rooms
+	 * {@link edu.cpp.cs.cs141.classproject.Room#getHasDocument()} as
+	 * {@code true}
+	 */
+	public void generateRooms() {
+	}
+
+	/**
+	 * @return An integer that will be put into a switch that will determine the
+	 *         correct UI output for the given case.
+	 */
+	public int turn() {
+		return 0;
+	}
+
+	/**
+	 * Exports a file
+	 */
+	public void saveGame() {
+	}
+
+	/**
+	 * @param file
+	 */
+	public void loadGame(String file) {
 	}
 }
