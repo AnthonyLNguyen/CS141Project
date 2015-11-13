431b142e0d13ee4f46cd2727c9464ead472989e5
Thingon <anthonylnguyen97@gmail.com>
2015-11-12 22:28:11 -0800

    testGenerateMap();

--- src/edu/cpp/cs/cs141/classprojecttests/Testerinos.java
+++ src/edu/cpp/cs/cs141/classprojecttests/Testerinos.java
@@ -5,7 +5,7 @@ import edu.cpp.cs.cs141.classproject.*;
 public class Testerinos {
 	
 	public static void main(String[] args) {
-		testMapMove();
+		testGenerateMap();
 	}
 	
 	public static void testMapPrint(){
