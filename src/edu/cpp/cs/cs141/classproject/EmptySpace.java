f05d3e1c60af3d599d5cd4feb4a8738226443d08
Bryan <Bryan@134.71.151.63>
2015-11-22 17:03:46 -0800

    added emptyspace class

---
+++ src/edu/cpp/cs/cs141/classproject/EmptySpace.java
@@ -0,0 +1,26 @@
+package edu.cpp.cs.cs141.classproject;
+
+public class EmptySpace {
+	
+	private boolean isHidden = true;
+	
+	public void setHidden(boolean isHidden) {
+		this.isHidden = isHidden;
+	}
+
+	public boolean isHidden() {
+		return isHidden;
+	}
+
+	/*
+	 * this show Ninja to the Map
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		if (isHidden)
+			return "*";
+		else
+			return " ";
+	}
+}
