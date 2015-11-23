cf1502d3c2ef66a225fd8cd9776e8ce7b783e7c2
Bryan <Bryan@134.71.151.63>
2015-11-22 17:14:16 -0800

    < 9

--- src/edu/cpp/cs/cs141/classproject/Game.java
+++ src/edu/cpp/cs/cs141/classproject/Game.java
@@ -199,8 +199,8 @@ public class Game {
 	 */
 	public void generateMap() {
 		
-		for (int i = 0; i <= 8; i++)
-			for (int j = 0; j <= 8; j++)
+		for (int i = 0; i < 9; i++)
+			for (int j = 0; j < 9; j++)
 				gameMap.addObject(i, j, new EmptySpace());
 
 		gameMap.addObject(player.getRow(), player.getCol(), player);
